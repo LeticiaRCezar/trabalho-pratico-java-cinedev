88c59f51e7308cd55e119676915c795a8fd2e0d8
LeticiaRCezar <leticia.cezar@aluno.senai.br>
2025-09-17 22:04:19 -0300

    Criação da classe CancelamentoIngresso e método para liberar assentos

---
+++ src/CancelamentoIngresso.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CancelamentoIngresso {
+    
+    private MapaAssentos mapa;
+    private Scanner scanner;
+    
+    public CancelamentoIngresso (MapaAssentos mapa, Scanner scanner) {
+        this.mapa = mapa;
+        this.scanner = scanner;   
+    }
+    
+    public void cancelar() {
+        System.out.println("Fileira (1-10): ");
+        int f = scanner.nextInt() - 1;
+        System.out.println("Assento (1-20): ");
+        int a = scanner.nextInt() - 1;
+        
+        if (mapa.ocupar(f, a)) {
+            System.out.println("Compra Cancelada!");
+        } else {
+            System.out.println("Esse assento está livre!");
+        }
+    }
+}
