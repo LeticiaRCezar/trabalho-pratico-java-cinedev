18a3516325a709ab04cd3db0650493e373481181
LeticiaRCezar <leticia.cezar@aluno.senai.br>
2025-09-18 23:16:17 -0300

    Correção de código: Alterado método ocupar() para liberar assento

--- src/CancelamentoIngresso.java
+++ src/CancelamentoIngresso.java
@@ -16,7 +16,7 @@ public class CancelamentoIngresso {
         System.out.println("Assento (1-20): ");
         int a = scanner.nextInt() - 1;
         
-        if (mapa.ocupar(f, a)) {
+        if (mapa.liberar(f, a)) {
             System.out.println("Compra Cancelada!");
         } else {
             System.out.println("Esse assento está livre!");
