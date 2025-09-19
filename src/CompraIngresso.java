ef81ed1c0ddb22f71db0c22457a256c47a0b7407
LeticiaRCezar <leticia.cezar@aluno.senai.br>
2025-09-19 17:19:39 -0300

    Ajustando layout de exibição na compra de ingressos e validação de seleção de assentos

--- src/CompraIngresso.java
+++ src/CompraIngresso.java
@@ -15,15 +15,28 @@ public class CompraIngresso {
     
     // Confirmação da compra de ingresso e aviso para erro de compra
     public void comprar() {
-        System.out.println("Fileira (1-10): ");
+        
+        System.out.println();
+        
+        System.out.println("------------------");
+        System.out.println("COMPRA DE INGRESSO");
+        System.out.println("------------------");  
+        
+        System.out.print("Fileira (1-10): ");
         int f = scanner.nextInt() - 1;
-        System.out.println("Assento (1-20): ");
+        System.out.print("Assento (1-20): ");
         int a = scanner.nextInt() - 1;
         
-        if (mapa.ocupar(f, a)) {
+        System.out.println();
+        
+        if (f < 0 || f >= mapa.Fileiras() || a < 0 || a >= mapa.Assentos()) {
+            System.out.println("Erro! Fileira ou assento inválido!");
+        } else if (mapa.ocupar(f, a)) {
             System.out.println("Ingresso Comprado!");
         } else {
             System.out.println("Esse assento já está ocupado!");
         }
+        
+        System.out.println();
     }
 }
