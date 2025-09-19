215bc144c3b72ebb810b029d1d2bd52206e3b838
LeticiaRCezar <leticia.cezar@aluno.senai.br>
2025-09-18 23:47:40 -0300

    Atualizando RelatorioOcupacao com comentários explicativos

--- src/RelatorioOcupacao.java
+++ src/RelatorioOcupacao.java
@@ -1,10 +1,14 @@
 public class RelatorioOcupacao {
+    
+    // Atributos da classe
     private MapaAssentos mapa;
     
+    // Recebe o mapa de assentos
     public RelatorioOcupacao(MapaAssentos mapa) {
         this.mapa = mapa;
     }
     
+    // Organizando a leitura do código e mostrando o relatório de ocupação 
     public void exibir() {
         int total = mapa.totalAssentos();
         int ocupados = mapa.ocupados();
